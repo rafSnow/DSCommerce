c56022a71ddbe2a609123d7a37b9b2936a6dcfa9
Rafael Neves <rafael.nsouza@goias.gov.br>
2024-02-05 11:40:33 -0300

    Adicionando metodo Insert

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -26,4 +26,15 @@ public class ProductService {
         Page<Product> result = repository.findAll(pageable);
         return result.map(x -> new ProductDTO(x));
     }
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgURL(dto.getImgURL());
+
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
 }
